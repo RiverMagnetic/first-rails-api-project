1ebfe17d1438d8ee5ab34f59909d758a8f41cdb0
RiverMagnetic <magneticcontinuum@gmail.com>
2018-07-25 09:27:24 -0400

    Add one-to-many relationship to serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :item_name, :description
-  has_one :user
-  # belongs_to :user might be better
+  belongs_to :user
+  
+  # def item_name
+  #   scope == object.user
+  # end
+
+  # def description
+  #   scope == object.user
+  # end
 end
